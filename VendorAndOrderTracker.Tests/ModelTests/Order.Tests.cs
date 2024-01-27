959a49fb6666833d418d7236fc7585b05119698d
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-01-26 16:07:23 -0800

    add price + passing test

--- VendorAndOrderTracker.Tests/ModelTests/Order.Tests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/Order.Tests.cs
@@ -11,7 +11,7 @@ namespace VendorAndOrderTracker.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Order title",  "Order description");
+      Order newOrder = new Order("Order title",  "Order description", 5);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -19,7 +19,7 @@ namespace VendorAndOrderTracker.Tests
     public void GetOrderTitle_ReturnsOrderTitle_String()
     {
       string title = "Order title";
-      Order newOrder = new Order(title,  "Order description");
+      Order newOrder = new Order(title,  "Order description", 5);
       string result = newOrder.Title;
       Assert.AreEqual(title, result);
     }
@@ -28,7 +28,7 @@ namespace VendorAndOrderTracker.Tests
     public void SetOrderTitle_SetOrderTitle_String()
     {
       string title = "Order title";
-      Order newOrder = new Order(title,  "Order description");
+      Order newOrder = new Order(title,  "Order description", 5);
       string updatedTitle = "New Order";
       newOrder.Title = updatedTitle;
       string result = newOrder.Title;
@@ -39,7 +39,7 @@ namespace VendorAndOrderTracker.Tests
     public void GetOrderDescription_ReturnsOrderDescription_String()
     {
       string description = "Order description";
-      Order newOrder = new Order("Order title", description);
+      Order newOrder = new Order("Order title", description, 5);
       string result = newOrder.Description;
       Assert.AreEqual(description, result);
     }
@@ -48,11 +48,20 @@ namespace VendorAndOrderTracker.Tests
     public void SetOrderDescription_ReturnsSetOrderDescription_String()
     {
       string description = "Order description";
-      Order newOrder = new Order("Order title", description);
+      Order newOrder = new Order("Order title", description, 5);
       string updatedDescription = "New order description";
       newOrder.Description = updatedDescription;
       string result = newOrder.Description;
       Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetOrderPrice_ReturnsOrderPrice_Int()
+    {
+      int price = 5;
+      Order newOrder = new Order("Order title", "Order description", price);
+      int result = newOrder.Price;
+      Assert.AreEqual(price, result);
     }
   }
 }
